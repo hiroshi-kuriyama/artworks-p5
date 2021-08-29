a8d2e77dbdc76351b4530c5d1a59fdaa3036058b
hiroshi-kuriyama <29877317+hiroshi-kuriyama@users.noreply.github.com>
2021-08-29 11:11:42 +0900

    Update: randomize triangle rotation when it is born

--- works/20210828_triangle_ivy/ivy.js
+++ works/20210828_triangle_ivy/ivy.js
@@ -1,13 +1,16 @@
 class Ivy {
-    constructor(x, y, side_len, col) {
-        // triangle vertexes
-        this.tri_v = [
-            createVector(x - side_len / 2, y)
-            , createVector(x + side_len, y)
-            , createVector(x, y - side_len * Math.sqrt(3))
-        ];
+    constructor(x, y, size, col) {
         this.col = col
         this.lifespan = 300;
+        // triangle vertexes
+        this.tri_v = []
+        let rnd = random(0, 2 * PI);
+        for (let i = 0; i < 3; i++) {
+            this.tri_v[i] = createVector(
+                x + size * sin((i + 1) * ((2 * PI) / 3) + rnd), 
+                y + size * cos((i + 1) * ((2 * PI) / 3) + rnd)
+                );
+        }
     }
 
     display() {
@@ -22,12 +25,14 @@ class Ivy {
 
     update() {
         let pick_id, v_tmp_old, v_tmp_new;
-        pick_id = parseInt(2 * Math.random());
+        pick_id = parseInt(2 * random());
         v_tmp_old = this.tri_v[pick_id];
         this.tri_v.splice(pick_id, 1);
         // Flip the triangle
-        v_tmp_new = p5.Vector.sub(p5.Vector.add(this.tri_v[0], this.tri_v[1]), v_tmp_old);
-        // v_tmp_new = v_tmp_new.mult(0.999);
+        v_tmp_new = p5.Vector.sub(
+            p5.Vector.add(this.tri_v[0], this.tri_v[1]),
+             v_tmp_old
+             );
         this.tri_v.push(v_tmp_new);
 
         this.lifespan -= 1
--- works/20210828_triangle_ivy/sketch.js
+++ works/20210828_triangle_ivy/sketch.js
@@ -1,12 +1,13 @@
-let colors = ['#f0101c', '#f05697', '#0b469b', '#32b6c3', '#f78000', '#fddf0e', '#9fe063', '#315423', '#16141b'];
+let colors = ['#09E318', '#B4ED0C', '#D4B000', '#EB900C', '#E63901', '#05785B', '#695700', '#803906', '#820750', '#1E007D'];
+let SIZE = 4;
 let ivy_arr = [];
 
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(windowWidth, windowHeight);
     background(200);
 
-    let col = color(random(colors));
-    ivy_arr[0] = new Ivy(width / 2, height / 2, 4, col);
+    // let col = color(random(colors));
+    // ivy_arr[0] = new Ivy(width / 2, height / 2, 4, col);
 }
 
 function draw() {
@@ -17,9 +18,14 @@ function draw() {
             ivy_arr.splice(i, 1);
         }
     }
+
+    if (frameCount % 10 == 0) {
+        let col = color(random(colors));
+        ivy_arr.push(new Ivy(parseInt(width * random()), parseInt(height * random()), SIZE, col));
+    }
     
     if (mouseIsPressed) {
         let col = color(random(colors));
-        ivy_arr.push(new Ivy(mouseX, mouseY, 4, col));
+        ivy_arr.push(new Ivy(mouseX, mouseY, SIZE, col));
     }
 }
