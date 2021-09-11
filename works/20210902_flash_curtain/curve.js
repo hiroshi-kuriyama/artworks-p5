57f4d090d8dffd7ff911db0f4ce27c2cdc5d3c38
hiroshi-kuriyama <29877317+hiroshi-kuriyama@users.noreply.github.com>
2021-09-11 10:36:51 +0900

    Update: apply neon effect

--- works/20210902_flash_curtain/curve.js
+++ works/20210902_flash_curtain/curve.js
@@ -19,7 +19,13 @@ class Curve {
         let angle = this.angle_init + i * this.angular_velocity;
         let pos_x = this.init_x + (60-i) * noise(i/10) * sin(angle);
         let pos_y = this.init_y + i * 10;
-        ellipse(pos_x, pos_y, (60-i)/10);
+        noStroke();
+        // fill(211, 135, 189, 80);
+        // ellipse(pos_x, pos_y, 3 * (60-i) / 10);
+        // fill(245, 203, 232, 100);
+        // ellipse(pos_x, pos_y, 2 * (60-i) / 10);
+        fill(255);
+        ellipse(pos_x, pos_y, 2 * (60-i) / 10);
     }
 
     // isDead() {
--- works/20210902_flash_curtain/sketch.js
+++ works/20210902_flash_curtain/sketch.js
@@ -8,17 +8,23 @@ function setup() {
 	background(0);
     // noiseSeed(9999);
 	for (let i = 0; i < NUM_CURVES; i++) {
-		let init_x = i * width / NUM_CURVES;
+		let init_x = i * width / NUM_CURVES + random(0, width / NUM_CURVES);
 		curves[i] = new Curve(init_x, 0);
 	}
 }
 
 
 function draw() {
-	background(0, 50);
+	background(0);
+	// https://infosmith.biz/blog/it/shadow_glow_p5js_1
+	drawingContext.shadowOffsetX = 0;
+	drawingContext.shadowOffsetY = 0;
+	drawingContext.shadowBlur = 60;
+	drawingContext.shadowColor = '#00FFAA';
 	fill(200);
 	for (let i = 0; i < curves.length; i++) {
 		curves[i].update();
 		curves[i].display();
 	}
 }
+
