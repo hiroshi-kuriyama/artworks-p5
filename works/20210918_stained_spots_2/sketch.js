9dd171cf72f84ae5fce2b96737cab2be26d46759
hiroshi-kuriyama <29877317+hiroshi-kuriyama@users.noreply.github.com>
2021-09-18 20:57:31 +0900

    Update: use mousePressed(). If cliked, generate a stain.

--- works/20210918_stained_spots_2/sketch.js
+++ works/20210918_stained_spots_2/sketch.js
@@ -1,22 +1,26 @@
 let color_palette = ['#FBFFB9', '#fffff5', '#2b90d9', '#a6172d', '#FADAD8'];
 // 黄色、白、水色、深紅、淡いピンク
 
-// let CENTER = [400, 400];
+let stains = [];
+
 function setup() {
     createCanvas(800, 800);
     background('#08101E');
+    // let CENTER = [400, 400];
     // let stain = new Stain(CENTER, 600, color('yellow'));
     // stain.display();
 }
 
-let num = 10;
-
 function draw(){
-    if(frameCount % 60 == 1) {
-        let center = [random(width), random(height)];
-        let size = random(50, 600);
-        let col = random(color_palette);
-        let stain = new Stain(center, size, col);
-        stain.display();
+    if(stains.length > 0) {
+        stains[0].display();
+        stains.shift();
     }
+}
+
+function mousePressed() {
+    let center = [mouseX, mouseY];
+    let size = random(50, 600);
+    let col = random(color_palette);
+    stains.push(new Stain(center, size, col));
 }
