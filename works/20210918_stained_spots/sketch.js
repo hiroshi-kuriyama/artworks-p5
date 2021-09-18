d42b8a61e0ac81eceab8539e899be730d931d53f
hiroshi-kuriyama <29877317+hiroshi-kuriyama@users.noreply.github.com>
2021-09-18 15:09:41 +0900

    Update: make Stain with bezier curves

--- works/20210918_stained_spots/sketch.js
+++ works/20210918_stained_spots/sketch.js
@@ -1,7 +1,10 @@
 
-let t = 0;
 let NOISE_SCALE = 10;
-let RESOLUTION = 100
+let RESOLUTION = 100;
+let CENTER = [400, 400];
+let NUM_VERTEX = 3
+let LEN_OUTER = 100;
+let LEN_INNER = 20;
 let p1 = [200, 600];
 let p2 = [400, 200];
 let p3 = [600, 600];
@@ -9,8 +12,30 @@ let p3 = [600, 600];
 function setup() {
     createCanvas(800, 800);
     background(0);
-    plot_control_points(p1, p2, p3);
-    bezier_curve(p1, p2, p3);
+    let theta_init = random(2 * PI);
+    let outer_points = [];
+    let inner_points = [];
+    let theta_tmp = theta_init;
+    while (theta_tmp < theta_init + 2 * PI) {
+        let x_o, y_o, x_i, y_i;
+        x_o = CENTER[0] + LEN_OUTER * cos(theta_tmp);
+        y_o = CENTER[1] + LEN_OUTER * sin(theta_tmp);
+        outer_points.push([x_o, y_o]);
+        let theta_tmp_next, theta_inner;
+        theta_tmp_next = theta_tmp + random(2 * PI / NUM_VERTEX);
+        theta_inner = random(theta_tmp, theta_tmp_next);
+        x_i = CENTER[0] + LEN_INNER * cos(theta_inner);
+        y_i = CENTER[1] + LEN_INNER * sin(theta_inner);
+        inner_points.push([x_i, y_i]);
+        theta_tmp = theta_tmp_next;
+    }
+    for (let i = 0; i < outer_points.length - 1; i++) {
+        plot_control_points(outer_points[i], inner_points[i], outer_points[i + 1]);
+        bezier_curve(outer_points[i], inner_points[i], outer_points[i + 1]);
+    }
+    // connect the last dot
+    plot_control_points(outer_points[outer_points.length - 1], inner_points[outer_points.length - 1], outer_points[0]);
+    bezier_curve(outer_points[outer_points.length - 1], inner_points[outer_points.length - 1], outer_points[0]);
 }
 
 function draw() {
