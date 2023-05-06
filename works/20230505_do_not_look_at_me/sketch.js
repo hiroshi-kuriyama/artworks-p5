e0e528ee2a9932b1fa42e9f61327d2c6738f6af8
hiroshi-kuriyama <29877317+hiroshi-kuriyama@users.noreply.github.com>
2023-05-06 23:54:55 +0900

    base code for eye design

--- works/20230505_do_not_look_at_me/sketch.js
+++ works/20230505_do_not_look_at_me/sketch.js
@@ -33,6 +33,10 @@ function faceReady() {
   faceapi.detect(gotFaces);// Start detecting faces: 顔認識開始
 }
 
+function draw(){
+    drawBoxs(detections);//Draw detection box: 顔の周りの四角の描画
+}
+
 // Got faces: 顔を検知
 function gotFaces(error, result) {
   if (error) {
@@ -43,20 +47,31 @@ function gotFaces(error, result) {
   detections = result;　//Now all the data in this detections: 全ての検知されたデータがこのdetectionの中に
 //   console.log(detections);
 
-  drawBoxs(detections);//Draw detection box: 顔の周りの四角の描画
-
   faceapi.detect(gotFaces);// Call the function again at here: 認識実行の関数をここでまた呼び出す
 }
 
 function drawBoxs(detections){
   if (detections.length > 0) {//If at least 1 face is detected: もし1つ以上の顔が検知されていたら
     clear();
+    tmpShowVideo();
     for (f=0; f < detections.length; f++){
       let {_x, _y, _width, _height} = detections[f].alignedRect._box;
       stroke(44, 169, 225);
       strokeWeight(1);
       noFill();
+      drawEye(_x, _y, _width, _height);
       rect(width - (_x + _width), _y, _width, _height);
     }
   }
+}
+
+function drawEye(_x, _y, _width, _height){
+    circle(width - (_x + _width /  2), _y + _height / 2, 100);
+}
+
+function tmpShowVideo(){
+    push();
+    scale(-1, 1);  // Flip the video horizontally
+    image(video, -width, 0, width, height);
+    pop();
 }
