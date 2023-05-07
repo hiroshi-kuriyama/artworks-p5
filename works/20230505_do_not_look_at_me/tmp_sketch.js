95e03f5a15407215dbfa3623a0a2c43e19605475
hiroshi-kuriyama <29877317+hiroshi-kuriyama@users.noreply.github.com>
2023-05-07 21:46:35 +0900

    fill color in eyelids

--- works/20230505_do_not_look_at_me/tmp_sketch.js
+++ works/20230505_do_not_look_at_me/tmp_sketch.js
@@ -13,13 +13,22 @@ function setup(){
 
     // circle(width/2, height/2, 40);
 
+
     eye_width = width * (2 / 3);
+    /* Outer line of eyelids */
+    noStroke();
+    fill(255, 0, 0);
+    arc(width/2, (height-eye_width)/2, eye_width*2/sqrt(2), eye_width*2/sqrt(2), (1/4)*PI, (3/4)*PI, OPEN);
+    fill(0, 255, 0);
+    arc(width/2, (height+eye_width)/2, eye_width*2/sqrt(2), eye_width*2/sqrt(2), (5/4)*PI, (7/4)*PI, OPEN);
 
-    arc(width/2, (height-eye_width*sqrt(3))/2, eye_width*2, eye_width*2, (1/3)*PI, (2/3)*PI);
-    arc(width/2, (height+eye_width*sqrt(3))/2, eye_width*2, eye_width*2, (4/3)*PI, (5/3)*PI);
+    /* Inner line of eyelids */
+    erase();
+    arc(width/2, (height-eye_width*sqrt(3))/2, eye_width*2, eye_width*2, (1/3)*PI, (2/3)*PI, OPEN);
+    arc(width/2, (height+eye_width*sqrt(3))/2, eye_width*2, eye_width*2, (4/3)*PI, (5/3)*PI, OPEN);
+    noErase();
 
-    arc(width/2, (height-eye_width)/2, eye_width*2/sqrt(2), eye_width*2/sqrt(2), (1/4)*PI, (3/4)*PI);
-    arc(width/2, (height+eye_width)/2, eye_width*2/sqrt(2), eye_width*2/sqrt(2), (5/4)*PI, (7/4)*PI);
+
 
 
 
