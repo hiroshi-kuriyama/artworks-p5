ed362b9374ad72e1d0d2087ecbdcef8dd6a1ef1c
hiroshi-kuriyama <29877317+hiroshi-kuriyama@users.noreply.github.com>
2023-05-12 12:08:17 +0900

    add comments on sketch.js

--- works/20230505_do_not_look_at_me/sketch.js
+++ works/20230505_do_not_look_at_me/sketch.js
@@ -37,8 +37,8 @@ function setup() {
 
   /* Eye design */
   eye_width = width * (2 / 3);
-  pg_1 = createGraphics(width, height);
-  pg_2 = createGraphics(width, height);
+  pg_1 = createGraphics(width, height);// for drawPupil()
+  pg_2 = createGraphics(width, height);// for drawEyelids()
 
   palette = shuffle(palette, true);
   pupil_l_color_num = 5;
@@ -82,14 +82,14 @@ function drawBoxs(detections){
     background(0);
 
     // tmpShowVideo();
+
     let {_x, _y, _width, _height} = detections[0].alignedRect._box;
     _x = width - (_x + _width);// Flip the bounding box position horizontally like a mirror
+
     /* pupil */
-    // pupil_center_x = mouseX;
-    // pupil_center_y = mouseY;
     face_center_x = _x + _width / 2;
     face_center_y = _y + _height / 2;
-    pupil_center_x =  width/2 + (face_center_x/width-1/2)*eye_width*(sqrt(2)-1);
+    pupil_center_x =  width/2 + (face_center_x/width-1/2)*eye_width*(sqrt(2)-1);// Restrict the area pupil position can move.
     pupil_center_y = height/2 + (face_center_y/height-1/2)*eye_width*(sqrt(2)-1);
     drawPupil(pupil_center_x, pupil_center_y);
     /* eyelids */
@@ -101,14 +101,13 @@ function drawPupil(pupil_center_x, pupil_center_y){
   /* pupil layer 1 */
   pg_1.clear();
   pg_1.noStroke();
-  for(i=0;i<pupil_l1_bp.length-1;i++){
+  for(i=0;i<pupil_l1_bp.length-1;i++){// make a circle like pizza cut
       pg_1.fill(palette[i]);
       pg_1.arc(pupil_center_x, pupil_center_y, eye_width*(sqrt(2)-1), eye_width*(sqrt(2)-1), pupil_l1_bp[i], pupil_l1_bp[i+1]);
   }
 
   /* pupil layer 2 */
-
-  for(i=0;i<pupil_l2_bp.length-1;i++){
+  for(i=0;i<pupil_l2_bp.length-1;i++){// make a circle like pizza cut smaller than layer 1
       pg_1.fill(palette[i+pupil_l_color_num]);
       pg_1.arc(pupil_center_x, pupil_center_y, eye_width*(sqrt(2)-1)*2/3, eye_width*(sqrt(2)-1)*2/3, pupil_l2_bp[i], pupil_l2_bp[i+1]);
   }
@@ -123,15 +122,15 @@ function drawEyelids(){
   pg_2.background(0);
   /* Outer line of eyelids */
   pg_2.noStroke();
-  pg_2.fill(palette[palette.length-1]);
+  pg_2.fill(palette[palette.length-1]);// Upper eyelid
   pg_2.arc(width/2, (height-eye_width)/2, eye_width*sqrt(2), eye_width*sqrt(2), (1/4)*PI, (3/4)*PI, OPEN);
-  pg_2.fill(palette[palette.length-2]);
+  pg_2.fill(palette[palette.length-2]);// Lower eyelid
   pg_2.arc(width/2, (height+eye_width)/2, eye_width*sqrt(2), eye_width*sqrt(2), (5/4)*PI, (7/4)*PI, OPEN);
 
   /* Inner line of eyelids */
   pg_2.erase();
-  pg_2.arc(width/2, (height-eye_width*sqrt(3))/2, eye_width*2, eye_width*2, (1/3)*PI, (2/3)*PI, OPEN);
-  pg_2.arc(width/2, (height+eye_width*sqrt(3))/2, eye_width*2, eye_width*2, (4/3)*PI, (5/3)*PI, OPEN);
+  pg_2.arc(width/2, (height-eye_width*sqrt(3))/2, eye_width*2, eye_width*2, (1/3)*PI, (2/3)*PI, OPEN);// Upper eyelid
+  pg_2.arc(width/2, (height+eye_width*sqrt(3))/2, eye_width*2, eye_width*2, (4/3)*PI, (5/3)*PI, OPEN);// Lower eyelid
   pg_2.noErase();
 
   image(pg_2, 0, 0);
